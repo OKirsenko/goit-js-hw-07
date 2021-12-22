6a0e8fcbd225564515b8549bd9ed92c15fe20a0e
OKirsenko <senka3434@gmail.com>
2021-12-22 11:55:22 +0200

    ex 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createImageGallery = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `     
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join("");
+};
+
+const onImageClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.tagName !== "IMG") return;
+
+  const originalImg = event.target.dataset.source;
+  const modalImg = basicLightbox.create(
+    `<img width="1400" height="900" src="${originalImg}">`
+  );
+  modalImg.show();
+
+  window.addEventListener("keydown", closeModal);
+
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      modalImg.close();
+      window.removeEventListener("keydown", closeModal);
+    }
+  }
+};
+
+gallery.insertAdjacentHTML("beforeend", createImageGallery());
+
+gallery.addEventListener("click", onImageClick);
